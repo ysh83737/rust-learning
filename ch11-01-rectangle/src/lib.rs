068dcc2bc3b109e0f77933639fa231a7c8db2b5f
Shawn Yuan <ysh83737@hotmail.com>
2024-01-04 22:23:38 +0800

    ch11-01 test `false`result with `assert!` macro

--- ch11-01-rectangle/src/lib.rs
+++ ch11-01-rectangle/src/lib.rs
@@ -25,4 +25,18 @@ mod tests {
 
         assert!(larger.can_hold(&smaller));
     }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 6,
+            height: 6,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 5,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
 }
