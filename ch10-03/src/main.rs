5ddf93a3827ca728764670b02da4cf366d3f3e18
Shawn Yuan <ysh83737@hotmail.com>
2024-01-04 09:56:01 +0800

    ch10-03 lifetime annotations in functions

--- ch10-03/src/main.rs
+++ ch10-03/src/main.rs
@@ -8,13 +8,29 @@
 
 //     println!("r: {}", r);
 // }
+// fn main() {
+//     let r;
+
+//     {
+//         let x = 5;
+//         r = x;
+//     }
+
+//     println!("r: {}", r);
+// }
+
 fn main() {
-    let r;
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
 
-    {
-        let x = 5;
-        r = x;
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
-
-    println!("r: {}", r);
 }
