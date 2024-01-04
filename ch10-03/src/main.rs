1100174a47c8c2e1c333dd974a9e30b7cf375311
Shawn Yuan <ysh83737@hotmail.com>
2024-01-04 09:47:25 +0800

    ch10-03 dangling reference

---
+++ ch10-03/src/main.rs
@@ -0,0 +1,20 @@
+// fn main() {
+//     let r;
+
+//     {
+//         let x = 5;
+//         r = &x;
+//     }
+
+//     println!("r: {}", r);
+// }
+fn main() {
+    let r;
+
+    {
+        let x = 5;
+        r = x;
+    }
+
+    println!("r: {}", r);
+}
