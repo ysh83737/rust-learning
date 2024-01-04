3071ef2ce2c54aa3354210edd6c418907bd6f234
Shawn Yuan <ysh83737@hotmail.com>
2024-01-04 10:23:04 +0800

    ch10-03 full demo

--- ch10-03/src/main.rs
+++ ch10-03/src/main.rs
@@ -35,17 +35,44 @@
 //     }
 // }
 
-#[derive(Debug)]
-struct ImportantExcerpt<'a> {
-    part: &'a str,
-}
+// #[derive(Debug)]
+// struct ImportantExcerpt<'a> {
+//     part: &'a str,
+// }
+
+// fn main() {
+//     let novel = String::from("Call me Ishmael. Some years ago...");
+//     let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+//     let i = ImportantExcerpt {
+//         part: first_sentence
+//     };
+
+//     println!("i = {:?}", i);
+// }
+
+use std::fmt::Display;
 
 fn main() {
-    let novel = String::from("Call me Ishmael. Some years ago...");
-    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
-    let i = ImportantExcerpt {
-        part: first_sentence
-    };
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+    let string3 = String::from("Hello world");
 
-    println!("i = {:?}", i);
+    let result = longest_with_an_announcement(string1.as_str(), string2, string3);
+    println!("The longest string is {}", result);
+}
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("ann = {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
