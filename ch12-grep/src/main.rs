38ef83c66769779058011f9dd6f5661a20747f79
Shawn Yuan <ysh83737@hotmail.com>
2024-01-08 09:40:33 +0800

    ch12-03 extracting logic to `fn run`

--- ch12-grep/src/main.rs
+++ ch12-grep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -12,10 +13,17 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let content = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    };
+}
 
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.file_path)?;
     println!("With text:\n{}", content);
+
+    Ok(())
 }
 
 struct Config {
