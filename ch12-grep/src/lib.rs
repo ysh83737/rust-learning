6fada3003782dd5574bd4091e780b4c2340fa966
Shawn Yuan <ysh83737@hotmail.com>
2024-01-09 20:27:08 +0800

    ch12-05 test case insensitive

--- ch12-grep/src/lib.rs
+++ ch12-grep/src/lib.rs
@@ -38,6 +38,10 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  vec![]
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -48,7 +52,23 @@ mod tests {
     let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
+
     assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+  }
+
+  #[test]
+  fn case_insensitive() {
+    let query = "rUsT";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+    assert_eq!(
+      vec!["Rust:", "Trust me."],
+      search_case_insensitive(query, contents),
+    );
   }
 }
