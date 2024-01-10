60be7a38a9ce4f8fc047cec4d6b97e6f9efc0537
Shawn Yuan <ysh83737@hotmail.com>
2024-01-10 09:56:12 +0800

    ch13-01 demo for FnMut closure

--- ch13-01-sort-by-key/src/main.rs
+++ ch13-01-sort-by-key/src/main.rs
@@ -14,12 +14,11 @@ fn main() {
     // list.sort_by_key(|r| r.width);
     // println!("{:#?}", list);
 
-    let mut sort_operations = vec![];
-    let value = String::from("by key called");
+    let mut num_sort_operations = 0;
 
     list.sort_by_key(|r| {
-        sort_operations.push(value);
+        num_sort_operations += 1;
         r.width
     });
-    println!("{:#?}", list);
+    println!("{:#?}, sorted in {num_sort_operations} operations", list);
 }
