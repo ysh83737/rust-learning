e43a13496bcf18e21c4aa68170a23538a3943459
Shawn Yuan <ysh83737@hotmail.com>
2024-01-10 09:26:56 +0800

    ch13-01 closure captures immutable reference

--- ch13-01-closure/src/main.rs
+++ ch13-01-closure/src/main.rs
@@ -1,6 +1,15 @@
 fn main() {
-    let example_closure = |x| x;
+    // let example_closure = |x| x;
 
-    let s = example_closure(String::from("Hello"));
-    let n = example_closure(5);
+    // let s = example_closure(String::from("Hello"));
+    // let n = example_closure(5);
+
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let only_borrows = || println!("From closure: {:?}", list);
+
+    println!("Before calling closure: {:?}", list);
+    only_borrows();
+    println!("After calling closure: {:?}", list);
 }
