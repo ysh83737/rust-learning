7b77737b0a29821e7d01432215817af49aba66b9
Shawn Yuan <ysh83737@hotmail.com>
2024-01-17 10:00:27 +0800

    ch15-04 Rc<T>

--- ch15-04-rc/src/main.rs
+++ ch15-04-rc/src/main.rs
@@ -1,12 +1,13 @@
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
+use std::rc::Rc;
 use crate::List::{ Cons, Nil };
 
 fn main() {
-    let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
-    let b = Cons(3, Box::new(a));
-    let c = Cons(4, Box::new(a));
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
 }
