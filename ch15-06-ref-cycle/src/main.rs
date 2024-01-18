2abb2cb2d7735695de0e7dbd0deb64d54bb3f031
Shawn Yuan <ysh83737@hotmail.com>
2024-01-18 10:19:47 +0800

    ch15-06 normal reference

---
+++ ch15-06-ref-cycle/src/main.rs
@@ -0,0 +1,31 @@
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(&item),
+            Nil => None,
+        }
+    }
+}
+
+use std::rc::Rc;
+use std::cell::RefCell;
+use crate::List::{ Cons, Nil };
+
+fn main() {
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(4, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+}
