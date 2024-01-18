028799eeb2eead26e3ec52184ce2617e050bcd50
Shawn Yuan <ysh83737@hotmail.com>
2024-01-18 11:13:13 +0800

    ch15-06 strong and weak reference  counts

--- ch15-06-weak-ref/src/main.rs
+++ ch15-06-weak-ref/src/main.rs
@@ -17,29 +17,63 @@ fn main() {
 
     println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 
-    let branch = Rc::new(Node {
-        value: 5,
-        parent: RefCell::new(Weak::new()),
-        children: RefCell::new(vec![Rc::clone(&leaf)]),
-    });
+    println!("================================");
+    println!("leaf rc strong count = {}", Rc::strong_count(&leaf));
+    println!("leaf rc weak count = {}", Rc::weak_count(&leaf));
 
-    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+    
+        println!("================================");
+        println!("leaf rc strong count = {}", Rc::strong_count(&leaf));
+        println!("leaf rc weak count = {}", Rc::weak_count(&leaf));
+        println!("branch rc strong count = {}", Rc::strong_count(&branch));
+        println!("branch rc weak count = {}", Rc::weak_count(&branch));
+    
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+    
+        println!("================================");
+        println!("leaf rc strong count = {}", Rc::strong_count(&leaf));
+        println!("leaf rc weak count = {}", Rc::weak_count(&leaf));
+        println!("branch rc strong count = {}", Rc::strong_count(&branch));
+        println!("branch rc weak count = {}", Rc::weak_count(&branch));
+    
+        println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    }
 
-    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!("================================");
+    println!("leaf rc strong count = {}", Rc::strong_count(&leaf));
+    println!("leaf rc weak count = {}", Rc::weak_count(&leaf));
 
     // println!("leaf = {:?}", leaf);
     // println!("branch = {:?}", branch);
 
-    let leaf2 = Rc::new(Node {
-        value: 4,
-        parent: RefCell::new(Weak::new()),
-        children: RefCell::new(vec![])
-    });
+    // let leaf2 = Rc::new(Node {
+    //     value: 4,
+    //     parent: RefCell::new(Weak::new()),
+    //     children: RefCell::new(vec![])
+    // });
 
-    branch.children.borrow_mut().push(Rc::clone(&leaf2));
+    // println!("================================");
+    // println!("leaf2 rc strong count = {}", Rc::strong_count(&leaf2));
+    // println!("leaf2 rc weak count = {}", Rc::weak_count(&leaf2));
+    // println!("branch rc strong count = {}", Rc::strong_count(&branch));
+    // println!("branch rc weak count = {}", Rc::weak_count(&branch));
 
-    *leaf2.parent.borrow_mut() = Rc::downgrade(&branch);
-    println!("leaf2 parent = {:?}", leaf.parent.borrow().upgrade());
+    // branch.children.borrow_mut().push(Rc::clone(&leaf2));
+
+    // println!("================================");
+    // println!("leaf2 rc strong count = {}", Rc::strong_count(&leaf2));
+    // println!("leaf2 rc weak count = {}", Rc::weak_count(&leaf2));
+    // println!("branch rc strong count = {}", Rc::strong_count(&branch));
+    // println!("branch rc weak count = {}", Rc::weak_count(&branch));
+
+    // *leaf2.parent.borrow_mut() = Rc::downgrade(&branch);
+    // println!("leaf2 parent = {:?}", leaf.parent.borrow().upgrade());
 
     // println!("branch = {:?}", branch);
 }
