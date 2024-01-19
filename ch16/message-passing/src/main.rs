5ee06bd6e6b74f03bd6d53d4569f7fcacf969f3d
Shawn Yuan <ysh83737@hotmail.com>
2024-01-19 10:01:15 +0800

    ch16-02 sending multiple messages

--- ch16/message-passing/src/main.rs
+++ ch16/message-passing/src/main.rs
@@ -1,15 +1,37 @@
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
+    // let (tx, rx) = mpsc::channel();
+
+    // thread::spawn(move || {
+    //     let msg = String::from("Hello");
+    //     tx.send(msg).unwrap();
+    //     // println!("Sent: {msg}");
+    // });
+
+    // let received = rx.recv().unwrap();
+    // println!("Got: {}", received);
+
+
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-        let msg = String::from("Hello");
-        tx.send(msg).unwrap();
-        // println!("Sent: {msg}");
+        let msgs = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for msg in msgs {
+            tx.send(msg).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
-    let received = rx.recv().unwrap();
-    println!("Got: {}", received);
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
