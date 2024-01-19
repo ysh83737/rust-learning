a61ced03950925ff58f441f78ea36a0cf8b3d894
Shawn Yuan <ysh83737@hotmail.com>
2024-01-19 09:34:26 +0800

    ch16-01 wait spawned thread finish using `join`

--- ch16/spawn/src/main.rs
+++ ch16/spawn/src/main.rs
@@ -2,15 +2,19 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
+    let handler = thread::spawn(|| {
         for i in 1..10 {
             println!("Hi, number {} from spawned thread.", i);
             thread::sleep(Duration::from_millis(1));
         }
     });
 
+    // handler.join().unwrap();
+
     for i in 1..5 {
         println!("Hi, number {} from main thread.", i);
-        thread::sleep(Duration::from_millis(5));
+        thread::sleep(Duration::from_millis(1));
     }
+
+    handler.join().unwrap();
 }
