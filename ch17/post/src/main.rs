3229ae1fadfc3a9672f18dbb2c974c4480c32542
Shawn Yuan <ysh83737@hotmail.com>
2024-01-22 10:10:46 +0800

    ch17-03 encoding states and behavior as types

---
+++ ch17/post/src/main.rs
@@ -0,0 +1,20 @@
+use post::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+
+    let post = post.reject();
+
+    let post = post.request_review();
+    let post = post.approve();
+    let post = post.reject();
+
+    let post = post.request_review();
+    let post = post.approve();
+    let post = post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
