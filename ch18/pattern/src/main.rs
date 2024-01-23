3084ec7820cf80454151e486d235db57d90652db
Shawn Yuan <ysh83737@hotmail.com>
2024-01-23 21:30:16 +0800

    ch18-03 matching rangs of values

--- ch18/pattern/src/main.rs
+++ ch18/pattern/src/main.rs
@@ -22,4 +22,19 @@ fn main() {
     }
 
     println!("End, x = {:?}, y = {}", x, y);
+
+    let x = 5;
+
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
 }
