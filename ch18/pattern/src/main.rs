512823c4bcc1e1548ebc0d13e4b581154a8f70d2
Shawn Yuan <ysh83737@hotmail.com>
2024-01-23 21:24:19 +0800

    ch18-03 match expression's scope

--- ch18/pattern/src/main.rs
+++ ch18/pattern/src/main.rs
@@ -6,4 +6,15 @@ fn main() {
         3 => println!("three"),
         _ => println!("anything"),
     }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {}", y),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("End, x = {:?}, y = {}", x, y);
 }
