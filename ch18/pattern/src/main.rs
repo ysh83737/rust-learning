bbac500db7cdcaf57f508d112480f112ee85700b
Shawn Yuan <ysh83737@hotmail.com>
2024-01-23 21:50:31 +0800

    ch18-03 destructure struct

--- ch18/pattern/src/main.rs
+++ ch18/pattern/src/main.rs
@@ -37,4 +37,26 @@ fn main() {
         'k'..='z' => println!("late ASCII letter"),
         _ => println!("something else"),
     }
+
+    let p = Point {
+        x: 0,
+        y: 5,
+    };
+
+    let Point { x: a, y: b } = p;
+    println!("a = {}, b = {}", a, b);
+
+    let Point { x, y } = p;
+    println!("x = {}, y = {}", x, y);
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {x}"),
+        Point { x: 0, y } => println!("On the y axis at {y}"),
+        Point { x, y } => println!("On neither axis: ({x}, {y})"),
+    }
+}
+
+struct Point {
+    x: i32,
+    y: i32,
 }
