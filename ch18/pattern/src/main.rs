a715b955b67eb3c103abb2f587295780698594cd
Shawn Yuan <ysh83737@hotmail.com>
2024-01-23 21:50:47 +0800

    ch18-03 destructure enum

--- ch18/pattern/src/main.rs
+++ ch18/pattern/src/main.rs
@@ -54,9 +54,25 @@ fn main() {
         Point { x: 0, y } => println!("On the y axis at {y}"),
         Point { x, y } => println!("On neither axis: ({x}, {y})"),
     }
+
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => println!("The Quit variant has no data to destructure."),
+        Message::Move { x, y } => println!("Move in the x direction {x} and in the y direction {y}"),
+        Message::Write(text) => println!("Text message: {text}"),
+        Message::ChangeColor(r, g, b) => println!("Change the color to red {r}, green {g}, and blue {b}"),
+    }
 }
 
 struct Point {
     x: i32,
     y: i32,
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
