9f8b2124d3738a802da1105452e7a5b869a7f54f
Shawn Yuan <ysh83737@hotmail.com>
2024-01-24 10:00:21 +0800

    ch19-01 implement `split_at_mut` using unsafe api

--- ch19/unsafe-rust/src/main.rs
+++ ch19/unsafe-rust/src/main.rs
@@ -1,3 +1,5 @@
+use std::slice;
+
 fn main() {
     let mut num = 5;
 
@@ -11,8 +13,44 @@ fn main() {
         println!("r2 = {}", *r2);
         dangerous();
     }
+
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+    let r = &mut v[..];
+
+    let (a, b) = r.split_at_mut(3);
+    println!("a = {:?}", a);
+    println!("b = {:?}", b);
+    assert_eq!(a, [1, 2, 3]);
+    assert_eq!(b, [4, 5, 6]);
+
+
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+    let r = &mut v[..];
+
+    let (a, b) = split_at_mut(r, 3);
+    println!("a = {:?}", a);
+    println!("b = {:?}", b);
+    assert_eq!(a, [1, 2, 3]);
+    assert_eq!(b, [4, 5, 6]);
 }
 
 unsafe fn dangerous() {
     println!("dangerous fn");
+}
+
+fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+
+    assert!(mid <= len);
+
+    // (&mut values[..mid], &mut values[mid..])
+
+    let ptr = values.as_mut_ptr();
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
 }
