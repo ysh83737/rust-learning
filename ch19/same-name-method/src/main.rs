e57e3ff5a6e64ae311b78d6719c1da5ae1c8b22f
Shawn Yuan <ysh83737@hotmail.com>
2024-01-25 09:27:17 +0800

    ch19-02 fully qualified syntax

--- ch19/same-name-method/src/main.rs
+++ ch19/same-name-method/src/main.rs
@@ -26,10 +26,31 @@ impl Human {
     }
 }
 
+trait Animal {
+    fn baby_name() -> String;
+}
+
+struct Dog;
+
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
+    }
+}
+
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
+    }
+}
+
 fn main() {
     let person = Human;
 
     Pilot::fly(&person);
     Wizard::fly(&person);
     person.fly();
+
+    println!("A baby dog is called a {}", Dog::baby_name());
+    println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
 }
