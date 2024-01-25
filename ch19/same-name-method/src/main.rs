748ff0c78c35375b5421a74df657cbac963f9994
Shawn Yuan <ysh83737@hotmail.com>
2024-01-25 09:19:10 +0800

    ch19-02 impl methods with same name

---
+++ ch19/same-name-method/src/main.rs
@@ -0,0 +1,33 @@
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("Fly as `Pilot`");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Fly as `Wizard`");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("Fly as `Human`");
+    }
+}
+
+fn main() {
+    let person = Human;
+
+    person.fly();
+}
