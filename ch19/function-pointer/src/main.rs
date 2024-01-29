34d3a280196db06c5950caf03b6275dca8e31210
Shawn Yuan <ysh83737@hotmail.com>
2024-01-29 10:02:46 +0800

    ch19-04 function pointer

--- ch19/function-pointer/src/main.rs
+++ ch19/function-pointer/src/main.rs
@@ -18,4 +18,13 @@ fn main() {
 
     let list_of_strings: Vec<String> = list_of_numbers.iter().map(ToString::to_string).collect();
     println!("list_of_strings = {:?}", list_of_strings);
+
+    let list_of_status: Vec<Status> = (0u32..20).map(Status::Value).collect();
+    println!("list_of_status = {:?}", list_of_status);
+}
+
+#[derive(Debug)]
+enum Status {
+    Value(u32),
+    Stop,
 }
