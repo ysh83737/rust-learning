33b001a6be9b3c12e8ca74930b6ae023ff531e1c
Shawn Yuan <ysh83737@hotmail.com>
2024-01-29 11:46:18 +0800

    ch20-01 http response html

--- ch20/hello/src/main.rs
+++ ch20/hello/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::{prelude::*, BufReader};
 use std::net::{TcpListener, TcpStream};
 
@@ -21,7 +22,14 @@ fn handle_connection(mut stream: TcpStream) {
         .collect();
     // println!("Request: {:#?}", http_request);
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    // let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    let status_line = "HTTP/1.1 200 OK";
+
+    let contents = fs::read_to_string("hello/hello.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
