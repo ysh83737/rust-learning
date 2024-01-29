b243969e1385741460a91e719b0642220b2bc7d4
Shawn Yuan <ysh83737@hotmail.com>
2024-01-29 09:59:51 +0800

    ch19-04 function pointer

--- ch19/function-pointer/src/main.rs
+++ ch19/function-pointer/src/main.rs
@@ -10,4 +10,12 @@ fn main() {
     let answer = add_twice(add_one, 1);
 
     println!("answer = {}", answer);
+
+    let list_of_numbers = vec![1, 2, 3, 4];
+    let list_of_strings: Vec<String> = list_of_numbers.iter().map(|x| x.to_string()).collect();
+
+    println!("list_of_strings = {:?}", list_of_strings);
+
+    let list_of_strings: Vec<String> = list_of_numbers.iter().map(ToString::to_string).collect();
+    println!("list_of_strings = {:?}", list_of_strings);
 }
